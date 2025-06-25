d11336256e83401b662961dc39288fda50ff19ee
Josh Bell <joshua.bell.828@gmail.com>
2025-06-25 19:26:20 -0400

    opacity

--- components/my-menu.tsx
+++ components/my-menu.tsx
@@ -194,7 +194,7 @@ export default function MyMenu() {
   return (
     <>
       {/* Desktop Floating Menu */}
-      <div className="hidden md:block fixed top-4 left-1/2 transform -translate-x-1/2 z-50">
+      <div className="hidden md:block fixed top-4 left-1/2 transform -translate-x-1/2 z-50 opacity-80">
         <Menubar
           className={cn(
             "bg-background/90 backdrop-blur-md border border-border/50 shadow-lg",
